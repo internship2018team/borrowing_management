3a9e23435f725c190c4e45c698e5886500209ccb
usushioxp <42565377+usushioxp@users.noreply.github.com>
2018-08-23 14:18:02 +0900

    Delete config.php

--- app/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-ini_set("display_errors", 1);
-error_reporting(E_ALL);
-//自分のDBNAMEに変更するのを忘れない
-define('DB_DATABASE', 'book_management');
-define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'root');
-define('PDO_DSN', 'mysql:host=mysql;dbname=' . DB_DATABASE);
