c227c025d79f639d0a0cb58beff4c3dc4da5cf70
HiroshiHayano <hiroshi.hayano@e-grant.net>
2018-08-28 14:29:46 +0900

    貸し出し可能、貸し出し不可能な本の表示、非表示を切り替えできる機能を追加

--- app/index.php
+++ app/index.php
@@ -41,60 +41,61 @@ $user_status = $book->getUser();
                 <input type="text" name="query" placeholder="検索">
             </form>
             <?php if(isset($_POST["query"])): ?>
-                <?php echo "検索結果 : " . $_POST["query"] ?>
+                <?php echo "検索結果 : 『" . $_POST["query"] . "』 " . count($book_status) . "件"?>
             <?php endif;?>
-            <form>
-                <input type="checkbox" name="state" checked>貸し出し可能
-                <input type="checkbox" name="state" checked>貸し出し不可
-            </form>
         </div>
         <div class="booklist">
+            <input type="checkbox" id="is_borrowable" checked>貸し出し可能
+            <input type="checkbox" id="isnot_borrowable" checked>貸し出し不可
             <table>
                 <thead><tr><th>タイトル</th><th>借りている人</th><th>返却期限</th><th>貸し出しor返却</th></tr></thead>
                 <tbody>
                     <!-- booklistの表示 -->
                     <?php foreach ($book_status as $book) : ?>
-                    <tr>
-                        <td id="title"><?php echo $book['title']; ?></td>
                         <!-- ステータスが貸出し可能かどうか -->
                         <!-- 貸し出し可能の場合 -->
                         <?php if($book['can_borrow']) : ?>
-                        <td>-</td>
-                        <td>-</td>
-                        <td>
-                        <form action="index.php" method="POST">
-                            <!-- <input type="text" name="user_id" ><br/> -->
-                            <select name="user_id">
-                                <?php foreach ($user_status as $user) : ?>
-                                <option value=<?php echo $user['id']; ?>><?php echo $user['name']; ?></option>
-                                <?php endforeach; ?>
-                            </select>
-                            <input type="hidden" name="book_id" value="<?= $book['book_id']; ?>">
-                            <input type="submit" value="借りる">
-                        </form>
-                        </td>
+                        <tr class='can_borrow'>
+                            <td id="title"><?php echo $book['title']; ?></td>
+                            <td>-</td>
+                            <td>-</td>
+                            <td>
+                            <form action="index.php" method="POST">
+                                <!-- <input type="text" name="user_id" ><br/> -->
+                                <select name="user_id">
+                                    <?php foreach ($user_status as $user) : ?>
+                                    <option value=<?php echo $user['id']; ?>><?php echo $user['name']; ?></option>
+                                    <?php endforeach; ?>
+                                </select>
+                                <input type="hidden" name="book_id" value="<?= $book['book_id']; ?>">
+                                <input type="submit" value="借りる">
+                            </form>
+                            </td>
+                        </tr>
                         <!-- 貸し出し不可の場合 -->
                         <?php else : ?>
-                        <form action="index.php" method="POST">
-                            <input type="hidden" name="history_id" value="<?= $book['history_id']; ?>">
-                            <?php $user_id = $book['user_id']; ?>
-                            <?php foreach ($user_status as $user) : ?>
-                                <?php if($user['id'] == $user_id) : ?>
-                                    <?php $limit_date = new Datetime($book['borrow_date']); ?>
-                                    <!-- 返却期限は２週間後 -->
-                                    <?php $limit_date = $limit_date->modify('+2 weeks'); ?>
-                                    <td><?php echo $user['name']; ?></td>
-                                    <?php if($limit_date > new Datetime(date("Y/m/d"))) : ?>
-                                        <td><?php echo $limit_date->format("Y/m/d(D)"); ?></td>
-                                    <?php else:?>
-                                        <td><font color='red'><?php echo $limit_date->format("Y/m/d(D)"); ?></font></td>
+                        <tr class="cannot_borrow">
+                            <td id="title"><?php echo $book['title']; ?></td>
+                            <form action="index.php" method="POST">
+                                <input type="hidden" name="history_id" value="<?= $book['history_id']; ?>">
+                                <?php $user_id = $book['user_id']; ?>
+                                <?php foreach ($user_status as $user) : ?>
+                                    <?php if($user['id'] == $user_id) : ?>
+                                        <?php $limit_date = new Datetime($book['borrow_date']); ?>
+                                        <!-- 返却期限は２週間後 -->
+                                        <?php $limit_date = $limit_date->modify('+2 weeks'); ?>
+                                        <td><?php echo $user['name']; ?></td>
+                                        <?php if($limit_date > new Datetime(date("Y/m/d"))) : ?>
+                                            <td><?php echo $limit_date->format("Y/m/d(D)"); ?></td>
+                                        <?php else:?>
+                                            <td><font color='red'><?php echo $limit_date->format("Y/m/d(D)"); ?></font></td>
+                                        <?php endif;?>
                                     <?php endif;?>
-                                <?php endif;?>
-                            <?php endforeach; ?>
-                            <td><input type="submit" value="返却"></td>
-                        </form>
+                                <?php endforeach; ?>
+                                <td><input type="submit" value="返却"></td>
+                            </form>
+                        </tr>
                         <?php endif; ?>
-                    </tr>
                     <?php endforeach; ?>
                 </tbody>                
             </table>
