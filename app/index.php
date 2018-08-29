7fb95413d536a26433b2f1f96cc770d73a215b0a
HiroshiHayano <hiroshi.hayano@e-grant.net>
2018-08-29 17:52:28 +0900

    [index.php]MDLよりデザインをお借りした

--- app/index.php
+++ app/index.php
@@ -33,8 +33,12 @@ $users = $bookApp->loadAllUsers();
         <meta http-equiv="X-UA-Compatible" content="ie=edge">
         <title>本の貸出し管理</title>
         <link rel="stylesheet" href="css/styles.css">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
+        <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.indigo-pink.min.css">
+        <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
     </head>
     <body>
+        <!-- ここに追加！ -->
         <a href="http://localhost:8080/app/index.php"><h1 class="page_name">BOOK LIST</h1></a>
         <div id="search_text">
             <form action="index.php" method="POST">
@@ -45,41 +49,52 @@ $users = $bookApp->loadAllUsers();
             <?php endif;?>
         </div>
         <div class="booklist">
-            <input type="checkbox" id="is_borrowable" checked>貸し出し可能
-            <input type="checkbox" id="isnot_borrowable" checked>貸し出し不可
-            <table>
-                <thead><tr><th>タイトル</th><th>借りている人</th><th>返却期限</th><th>貸し出しor返却</th></tr></thead>
+            <input type="checkbox" id="is_borrowable" class="mdl-checkbox__input" checked>
+            <label for="is_borrowable">貸し出し可能</label><br>
+            <input type="checkbox" id="isnot_borrowable" class="mdl-checkbox__input" checked>
+            <label for="isnot_borrowable">貸し出し中</label>
+            <table class="mdl-data-table mdl-js-data-table">
+                <thead>
+                    <tr>
+                        <th class="mdl-data-table__cell--non-numeric">タイトル</th>
+                        <th class="mdl-data-table__cell--non-numeric">借りている人</th>
+                        <th class="mdl-data-table__cell--non-numeric">返却期限</th>
+                        <th class="mdl-data-table__cell--non-numeric">貸し出しor返却</th>
+                    </tr>
+                </thead>
                 <tbody>
                     <!-- booklistの表示 -->
                     <?php foreach ($books as $book) : ?>
                         <?php if($book['can_borrow']) : ?>
                         <!-- 貸し出し可能の場合 -->
                             <tr class='can_borrow'>
-                                <td class="title"><?php echo $book['title']; ?></td>
-                                <td>-</td>
-                                <td>-</td>
-                                <td>
-                                    <form action="index.php" method="POST">
-                                        <select name="user_id">
+                                <td class="mdl-data-table__cell--non-numeric title"><?php echo $book['title']; ?></td>
+                                <form action="index.php" method="POST">
+                                    <td class="mdl-data-table__cell--non-numeric">
+                                        <select name="user_id" class="mdl-textfield__input">
+                                            <!-- 名前が表示されていると見辛い -->
                                             <?php foreach ($users as $user) : ?>
                                                 <option value=<?php echo $user['id']; ?>><?php echo $user['name']; ?></option>
                                             <?php endforeach; ?>
-                                        </select>
+                                        </select>                                    
+                                    </td>
+                                    <td class="mdl-data-table__cell--non-numeric">-</td>
+                                    <td class="mdl-data-table__cell--non-numeric">
                                         <input type="hidden" name="borrow_book_id" value=<?= $book['book_id']; ?>>
-                                        <input type="submit" value="貸し出し">
-                                    </form>
-                                </td>
+                                        <input type="submit" value="貸し出し" class="mdl-button mdl-js-button mdl-button--colored">
+                                    </td>
+                                </form>                                
                             </tr>
                         <?php else : ?>
                         <!-- 貸し出し中の場合 -->
                             <tr class="cannot_borrow">
-                                <td class="title"><?php echo $book['title']; ?></td>
+                                <td class="mdl-data-table__cell--non-numeric title"><?php echo $book['title']; ?></td>
                                 <?php $key = array_search($book['user_id'], array_column($users, 'id'))?>
                                 <?php if ($key === false) :?>
                                 <!-- データベースにUserが存在しないときは'No Name'で表記 -->
-                                    <td>No Name</td>
+                                    <td class="mdl-data-table__cell--non-numeric">No Name</td>
                                 <?php else:?>
-                                    <td><?php echo $users[$key]['name']; ?></td>
+                                    <td class="mdl-data-table__cell--non-numeric"><?php echo $users[$key]['name']; ?></td>
                                 <?php endif;?>
                                 <?php $limit_date = new Datetime($book['borrow_date']); ?>
                                 <!-- 返却期限は借りた日から2週間後 -->
@@ -87,17 +102,17 @@ $users = $bookApp->loadAllUsers();
                                 <?php $today = new Datetime(date("Y/m/d"))?>
                                 <?php if ($limit_date < $today) : ?>
                                 <!-- 返却期限を過ぎた -->
-                                    <td bgcolor='#FF6257'><?php echo $limit_date->modify('+9 hours')->format("Y/m/d(D)"); ?></td>
+                                    <td class="mdl-data-table__cell--non-numeric" bgcolor='#FF6257'><?php echo $limit_date->modify('+9 hours')->format("Y/m/d(D)"); ?></td>
                                 <?php elseif ($limit_date < $today->modify('+1 days')) : ?>
                                 <!-- 返却期限まで1日を切った -->
-                                    <td bgcolor='#FCFF6D'><?php echo $limit_date->modify('+9 hours')->format("Y/m/d(D)"); ?></td>
+                                    <td class="mdl-data-table__cell--non-numeric" bgcolor='#FCFF6D'><?php echo $limit_date->modify('+9 hours')->format("Y/m/d(D)"); ?></td>
                                 <?php else :?>
                                 <!-- 返却期限まで余裕がある -->
-                                    <td><?php echo $limit_date->modify('+9 hours')->format("Y/m/d(D)"); ?></td>
+                                    <td class="mdl-data-table__cell--non-numeric"><?php echo $limit_date->modify('+9 hours')->format("Y/m/d(D)"); ?></td>
                                 <?php endif;?>    
                                 <form action="index.php" method="POST">
                                     <input type="hidden" name="return_book_id" value=<?= $book['book_id']; ?>>
-                                    <td><input type="submit" value="返却"></td>
+                                    <td class="mdl-data-table__cell--non-numeric"><input type="submit" value="返却" class="mdl-button mdl-js-button"></td>
                                 </form>
                             </tr>
                         <?php endif; ?>
